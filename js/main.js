38c14b99d9019da8773f8145f31b724a2907794b
Prathamesh <prathprabhu@gmail.com>
2015-11-18 10:47:07 +0530

    The first structure created

---
+++ js/main.js
@@ -0,0 +1,65 @@
+//var game = function(){
+// // var a = 3;
+//  //var b = 3;
+// // var tile = [];
+// 
+// //   var row,cell;
+//  
+//var board = document.createElement("table");
+//    board.border=1;
+//  for (i = 0; i < 3; i += 1) {
+//           var row = document.createElement("tr");
+//            board.appendChild(row);
+//            for (j = 0; j < 3; j += 1) {
+//            var cell = document.createElement("td");
+//                row.appendChild(cell);
+//               }
+//  }
+//};
+
+var player = function(name){
+  this.name = name;
+  this.score = 0;
+}
+var players =[];
+
+players[0]= new player("prath");
+players[1] = new player("Surya");
+
+var tile = function(value){
+  this.value = value;
+ 
+};
+
+var game = function(){
+  this.rows = ['x','0','x'];
+  this.columns = 3;
+  var tiles = [];
+  //52 loops = 4 *13
+  
+  for(var i = 0; i< 4;i++)
+    {
+    for(var j = 0; j< this.rows.length;j++){
+        var tileObject = new tile(j+1, this.rows[j]);
+        tiles.push(tileObject);
+        }
+      
+     }
+
+  return tiles;
+};
+
+var mytitles = new game();
+
+var init = function() {
+
+      for (var i = 0; i < mytitles.length; i++) {
+        div = document.createElement('div');
+        
+        document.body.appendChild(div);
+    }
+   
+
+};
+
+init();
